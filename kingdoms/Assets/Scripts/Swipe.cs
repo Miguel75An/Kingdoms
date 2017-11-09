fe56408f70fed14827cf135d3cac42e1d37eeb77
Chin <justinfchin@gmail.com>
2017-11-08 22:13:06 -0500

    updated stuff

--- kingdoms/Assets/Scripts/Swipe.cs
+++ kingdoms/Assets/Scripts/Swipe.cs
@@ -1,18 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
-public class Swipe : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		Rigidbody rb = GetComponent<Rigidbody>();
-
-		rb.mass = 10f;
+	// This script will tell you which direction you swiped in
+	public class Swipe : MonoBehaviour
+	{
 	
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
